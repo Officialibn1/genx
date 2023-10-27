81cad0f1be1d72c66075b9f5be4290754a2948fc
ibn <officialibn001@gmail.com>
2023-10-27 21:00:15 +0100

    cart page complete

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,9 +1,78 @@
 import React from 'react'
+import '../components/styles/CartPage.css'
+import { item } from '@/typings'
+import { notFound } from 'next/navigation'
+import Image from 'next/image'
+import { BsFillTrashFill, BsPlus } from 'react-icons/bs'
+import { BiMinus } from 'react-icons/bi'
 
-const CartPage = () => {
+const CartPage = async () => {
+    const res = await fetch(`https://dummyjson.com/products/category/laptops`)
+
+    const data = await res.json()
+
+    const items: item[] = await data.products
+
+    if (!items) {
+        notFound()
+    }
+
   return (
     <section className='usersCart'>
-        <h1>Users Carts</h1>
+        <div className="cartItems">
+            {items.map(item => (
+                <div key={item.id} className="cartItem">
+                    <div className="cartItemImg">
+                        <Image src={item.images[1]} width={150} height={100} quality={100} alt={item.title} />
+                    </div>
+                    <div className="cartItemDesc">
+                        <h1>
+                            {item.title}
+                        </h1>
+
+                        <div className="cartItemBtns">
+                            <button className="deleteItem">
+                                <BsFillTrashFill />
+                            </button>
+
+                            <div className="itemQuantity">
+                            <button>
+                                <BsPlus />
+                            </button>
+
+                            <p>( {5} )</p>
+
+                            <button>
+                                <BiMinus />
+                            </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+
+        <div className="cartTotal">
+            <h1>
+                Total Items: <p>{5}</p>
+            </h1>
+
+            <h1>
+                Sub-Total: <p>${23421.12}</p>
+            </h1>
+
+            <h1>
+                Shipping: <p>${131.12}</p>
+            </h1>
+
+            <h1>
+                Total: <p>${12331.121}</p>
+            </h1>
+
+            <button>
+                Order
+            </button>
+        </div>
     </section>
   )
 }
